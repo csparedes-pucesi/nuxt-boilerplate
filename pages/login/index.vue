98bfdb1ed96180454328b02975b8b28de66f5a34
Cristian Paredes <csparedes@pucesi.edu.ec>
2025-03-21 12:06:57 -0500

    checkpoint to add tankstack-query

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,54 +1,84 @@
-<template>
-	<div class="flex items-center justify-center min-h-screen">
-		<div
-			class="bg-white/80 p-8 rounded-xl shadow-2xl max-w-md w-full backdrop-blur-xl"
-		>
-			<h2 class="text-3xl font-bold mb-8 text-gray-800 text-center">
-				Bienvenido
-			</h2>
-			<form class="space-y-6">
-				<label
-					for="username"
-					class="block text-gray-800 text-sm font-semibold mb-2"
-				>
-					Nombre de Usuario
-				</label>
-				<UInput
-					icon="heroicons:user-circle"
-					size="xl"
-					variant="outline"
-					placeholder="Ingrese su usuario"
-					class="w-full rounded-lg"
-				/>
-				<label
-					for="password"
-					class="block text-gray-800 text-sm font-semibold mb-2"
-				>
-					Contraseña
-				</label>
-				<UInput
-					icon="heroicons:lock-closed"
-					size="xl"
-					variant="outline"
-					placeholder="Ingrese su usuario"
-					class="w-full rounded-lg"
-				/>
-
-				<UButton
-					type="button"
-					class="w-full flex items-center justify-center p-4"
-					label="Ingresar"
-					@click="$router.push('/dashboard')"
-				/>
-			</form>
-		</div>
-	</div>
-</template>
-
 <script lang="ts" setup>
+import type { FormSubmitEvent } from '@nuxt/ui'
+import z from 'zod'
+
 definePageMeta({
-	layout: 'login',
-});
+  layout: 'login',
+})
+
+const schema = z.object({
+  email: z.string({ message: 'Email required' }).email('Invalid email'),
+  password: z
+    .string({ message: 'Password required' })
+    .min(3, 'Must be at least 3 characters'),
+})
+
+type Schema = z.infer<typeof schema>
+
+const state = reactive<Partial<Schema>>({
+  email: undefined,
+  password: undefined,
+})
+
+const toast = useToast()
+const isLoading = ref(false)
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  console.log(event.data)
+  isLoading.value = true
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  toast.add({
+    title: 'Success',
+    description: 'You have successfully logged in',
+  })
+  isLoading.value = false
+}
 </script>
 
-<style></style>
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div
+      class="bg-white/80 p-8 rounded-xl shadow-2xl max-w-md w-full backdrop-blur-xl"
+    >
+      <h2 class="text-3xl font-bold mb-8 text-gray-800 text-center">
+        Bienvenido
+      </h2>
+      <UForm
+        :schema="schema"
+        :state="state"
+        class="space-y-6"
+        @submit.prevent="onSubmit"
+      >
+        <UFormField label="Usuario" name="email">
+          <UInput
+            v-model="state.email"
+            icon="heroicons:user-circle"
+            size="xl"
+            variant="outline"
+            placeholder="Ingrese su usuario"
+            class="w-full rounded-lg"
+          />
+        </UFormField>
+
+        <UFormField label="Contraseña" name="password">
+          <UInput
+            v-model="state.password"
+            type="password"
+            icon="heroicons:lock-closed"
+            size="xl"
+            variant="outline"
+            placeholder="Ingrese su usuario"
+            class="w-full rounded-lg"
+          />
+        </UFormField>
+
+        <UButton
+          type="submit"
+          class="w-full flex items-center justify-center p-4"
+          label="Ingresar"
+          :loading="isLoading"
+        />
+      </UForm>
+    </div>
+  </div>
+</template>
